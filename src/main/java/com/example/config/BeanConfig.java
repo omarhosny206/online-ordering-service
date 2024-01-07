c5293ac085b1ab1e5fca7d1de3ba9a6b3c00e800
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-07 23:53:57 +0200

    Add seedRoles bean method

--- src/main/java/com/example/config/BeanConfig.java
+++ src/main/java/com/example/config/BeanConfig.java
@@ -1,10 +1,17 @@
 package com.example.config;
 
+import com.example.entity.Role;
+import com.example.repository.RoleRepository;
+import com.example.util.Roles;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class BeanConfig {
@@ -16,5 +23,18 @@ public class BeanConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public CommandLineRunner seedRoles(RoleRepository roleRepository) {
+        return (args) -> {
+            List<String> rolesNames = Arrays.stream(Roles.values()).map(roles -> roles.getValue()).toList();
+            rolesNames.forEach(roleName -> {
+                Role storedRole = roleRepository.findByName(roleName);
+                if (storedRole == null) {
+                    roleRepository.save(new Role(roleName));
+                }
+            });
+        };
     }
 }
