6fb7c6c3eb42b46e41e0325e7a20e8807f0d8770
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-13 23:13:42 +0200

    Add cleanup.sh

---
+++ k8s/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Delete Kubernetes namespace and all resources within it
+kubectl delete namespace ingress-nginx
+
+# Delete other resources
+kubectl delete -f postgres.yaml
+kubectl delete -f online-ordering-service.yaml
+
+# Remove host entry from /etc/hosts
+INGRESS_NGINX_HOST="api.online-ordering-service.com"
+grep -v "$INGRESS_NGINX_HOST" /etc/hosts > /etc/hosts_temp
+cp /etc/hosts_temp /etc/hosts
+rm -f /etc/hosts_temp
+
+echo "Cleanup completed."
