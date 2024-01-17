d3e73d32a3d551fcf4b793a193ce1d70a4567fde
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-18 00:53:44 +0200

    Update deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -6,22 +6,17 @@ kubectl create namespace ingress-nginx
 # Deploy Ingress Nginx Controller
 kubectl apply -f ingress-nginx-controller.yaml
 
-# Wait for Ingress Nginx Controller service to get an external IP
-echo "Waiting for Ingress Nginx Controller to get an external IP..."
-while [ -z "$(kubectl get svc ingress-nginx-controller -n ingress-nginx -o jsonpath='{.spec.clusterIP}')" ]; do
-    sleep 5
-done
+# Wait for Ingress Nginx Controller to be fully ready
+echo "Waiting for Ingress Nginx Controller to be fully ready..."
+kubectl wait --namespace ingress-nginx --for=condition=available deployment/ingress-nginx-controller --timeout=300s
 
-# Get the IP address of Ingress Nginx Controller service
+# Get the ClusterIP of the Ingress Nginx Controller service
 INGRESS_NGINX_IP=$(kubectl get svc ingress-nginx-controller -n ingress-nginx -o jsonpath='{.spec.clusterIP}')
 
-# Deploy Postgres
-kubectl apply -f postgres.yaml
-
-# Deploy Tribe
-kubectl apply -f online-ordering-service.yaml
+# Deploy other resources
+kubectl apply -f .
 
 # Add host entry to /etc/hosts
-echo "$INGRESS_NGINX_IP api.online-ordering-service.com" | tee -a /etc/hosts
+echo "$INGRESS_NGINX_IP api.online-ordering-service.com" >> /etc/hosts
 
-echo "Setup completed."
+echo "Deployment has been done successfully."
